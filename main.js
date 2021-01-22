7e131c5d4740a98a98c42d9548f143a7ec5a6f06
rinahanyu <gravity0112@gmail.com>
2021-01-22 10:17:23 +0900

    j_query DOM

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-$('#box').text('Hello jQuery!');
+// DOM操作
 
-$(window).on('load', () => $('#box').text('画像などの読み込みも完了しました！'));
+// $('#box').text('Hello jQuery!');
+
+// loadイベント（ページ内のすべての外部CSSや画像の読み込みが完了したタイミングで発生するイベント）
+// $(window).on('load', () => $('#box').text('画像などの読み込みも完了しました！'));
+
+// 要素の追加
+// $('#box').append('<p>こんばんは</p>');
+
+// // 要素の削除
+// $('#box').remove();
+
+// // フォームのデータ受けとり
+// const value = $('#my-input').val();
+// console.log(value);
+
+// フォームの入力
+// const value = 'こんにちは';
+// $('#my-input').val(value);
+
+// 属性値を取得
+// const value = $('a').attr('href'); // 最初のa要素から、href属性の属性値を取得する
+// console.log(value);
+
+// 属性値の設定
+// $('a').attr('href', 'new.html'); //一致した全ての属性に対し、設定される
+
+// -------------------------------------------------------
+
+// イベント
+
